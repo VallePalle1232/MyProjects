51d8fdcc1613c3b530decf4de80ab37492985396
Valdemar <valpa404@student.liu.se>
2024-12-29 22:33:28 +0100

    Reorder files for visability

---
+++ Own/AttackMonitor/statuschecker.py
@@ -0,0 +1,17 @@
+import json
+import winsound
+import time 
+import requests
+
+
+def fetch_ip()->tuple:
+    addr = "" #ip address of our target
+    port = "5050"
+    return addr,port
+
+#checking "health" of computer using rest API
+
+def check_health():
+
+
+
